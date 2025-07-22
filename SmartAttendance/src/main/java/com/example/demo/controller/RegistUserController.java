db56832e57f0c570ac8591ebd372610b726c5219
github kazuma-satake <kazuma-satake@kccs.co.jp>
2025-07-22 16:23:25 +0900

    名前の変更

--- SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
+++ SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class TestController {
+public class RegistUserController {
 
 	private final RegistUserService service;
 
