d4491a93b559cec1deae66478518dabe15191bc4
github kazuma-satake <kazuma-satake@kccs.co.jp>
2025-07-22 16:54:26 +0900

    一部変更

--- SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
+++ SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
@@ -43,7 +43,6 @@ public class RegistUserController {
 	public String completeRegistUser(@ModelAttribute RegistUserForm form, Model model) {
 		service.RegistUser(form);
 		return "complete";
-
 	}
 
 }
