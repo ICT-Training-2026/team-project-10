f1ad7bb349c25c2850e27ad5a203aae36819835c
github kazuma-satake <kazuma-satake@kccs.co.jp>
2025-07-23 10:37:35 +0900

    新規ユーザ登録完成

--- SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
+++ SmartAttendance/src/main/java/com/example/demo/controller/RegistUserController.java
@@ -2,10 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.entity.LoginUser;
 import com.example.demo.form.RegistUserForm;
 import com.example.demo.service.RegistUserService;
 
@@ -17,17 +17,19 @@ public class RegistUserController {
 
 	private final RegistUserService service;
 
-	@GetMapping("/regist-user")
-	public String registUser(Model model) {
+	@PostMapping("/regist-user")
+	public String registUser(@ModelAttribute LoginUser loginUser, Model model) {
 		model.addAttribute("registUserForm", new RegistUserForm());
+		model.addAttribute("loginUser", loginUser);
 		return "regist-user";
 	}
 
 	@PostMapping("/confirm-regist-user")
-	public String cofirmRegistUser(@ModelAttribute RegistUserForm form, Model model) {
+	public String cofirmRegistUser(@ModelAttribute RegistUserForm form, @ModelAttribute LoginUser loginUser, Model model) {
 		Boolean check = service.UserCheck(form.getEmp_id());
 
 		model.addAttribute("registUserForm", form);
+		model.addAttribute("loginUser", loginUser);
 
 		if (!check) {
 			model.addAttribute("registUserForm", form);
@@ -40,8 +42,9 @@ public class RegistUserController {
 	}
 
 	@PostMapping("complete-regist-user")
-	public String completeRegistUser(@ModelAttribute RegistUserForm form, Model model) {
+	public String completeRegistUser(@ModelAttribute RegistUserForm form, @ModelAttribute LoginUser loginUser, Model model) {
 		service.RegistUser(form);
+		model.addAttribute("loginUser", loginUser);
 		return "complete";
 	}
 
