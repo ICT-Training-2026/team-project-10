317177c64bc57faaa59eb8c4205951137602518b
github kazuma-satake <kazuma-satake@kccs.co.jp>
2025-07-30 13:08:31 +0900

    バリデーションチェックの追加

--- SmartAttendance/src/main/java/com/example/demo/form/RegistKintaiForm.java
+++ SmartAttendance/src/main/java/com/example/demo/form/RegistKintaiForm.java
@@ -5,14 +5,14 @@ import java.sql.Date;
 import lombok.Data;
 @Data
 public class RegistKintaiForm {
-		private String emp_id;
-		private Date record_date;
-		private int shift_ID;
-		private int start_H;
-		private int start_M;
-		private int end_H;
-		private int end_M;
-		private double total_workingTime;
-		private int breakTime; /*休憩時間[時間]*/
-		private int excessTime; /*超過労働時間[時間]*/
+	private String emp_id;
+	private Date record_date;
+	private int shift_ID;
+	private int start_H;
+	private int start_M;
+	private int end_H;
+	private int end_M;
+	private double total_workingTime;
+	private int breakTime; /*休憩時間[時間]*/
+	private int excessTime; /*超過労働時間[時間]*/
 }
